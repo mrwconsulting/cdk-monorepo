f0a229a83a74c897cdd907774e6286dd707b1a6d
Mark Washburn <mark@mrwconsulting.tech>
2022-05-31 13:02:07 -0500

    chore: updated jest config

--- config/jest/jest.config.js
+++ config/jest/jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   verbose: true,
   testEnvironment: 'node',
   roots: ['../../packages/core/', '../../packages/patterns/'],
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
